07e1fc3633bd2863ac5886d9ea88c5c57c9d6549
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2024-12-16 15:45:28 +0100

    Continue inital process structure

--- src/cli-learning-cards.ts
+++ src/cli-learning-cards.ts
@@ -1,18 +1,32 @@
 import * as readline from "node:readline/promises";
 import { stdin, stdout } from "node:process";
 import { readFile } from "node:fs/promises";
-import type {SourceJson} from "./source-json.js";
+import type { Item, SourceJson } from "./source-json.js";
 
+/**
+ * Cli Learning cards main process.
+ * Read source, ask number of element to revise, prompt
+ * card and handle answers.
+ *
+ * TODO and ideas
+ * Finish first process implementation.
+ * Add a "__skip", "__clue" special arguments
+ * Add a possibility to add "cards" on demand/from time to time
+ */
 export class CliLearningCards {
   private rl?: readline.Interface;
   private cardsLimit = 0;
   private readonly sourcePath: URL;
   private sourceJson?: SourceJson;
+  private selectedItems: Item[] = [];
 
   constructor(sourcePath: URL) {
     this.sourcePath = sourcePath;
   }
 
+  /**
+   * Start the read-line stram and the questions processes.
+   */
   async run() {
     this.rl = readline.createInterface({ input: stdin, output: stdout });
     await this.readJsonSource();
@@ -21,13 +35,26 @@ export class CliLearningCards {
       this.stop();
     }
     await this.askNumberCards();
+    this.selectItems();
+    this.showQuestion(this.selectedItems[0]);
+    await this.readAnswer();
+    this.isCorrect();
+    this.showResult();
+    this.saveResult();
     this.stop();
   }
 
+  /**
+   * Close the read-line stream.
+   */
   stop() {
     this.rl?.close();
   }
 
+  /**
+   * Read the source json file.
+   * @private
+   */
   private async readJsonSource() {
     try {
       const contents = await readFile(this.sourcePath, { encoding: "utf8" });
@@ -37,6 +64,10 @@ export class CliLearningCards {
     }
   }
 
+  /**
+   * Ask how many item to show.
+   * @private
+   */
   private async askNumberCards() {
     const max = this.sourceJson?.items?.length ?? -1;
     const answer = parseInt(
@@ -51,22 +82,52 @@ export class CliLearningCards {
     }
   }
 
-  // Get a random item via a strategy
+  /**
+   * Get a random item via a strategy.
+   * @private
+   */
+  private selectItems() {
+    const items = [...(this.sourceJson?.items ?? [])];
+    // Default strategy is by last revision date.
+    items.sort((item1, item2) => +item1.last_revision - +item2.last_revision);
+    this.selectedItems = items.slice(0, this.cardsLimit);
+    console.log(this.selectedItems.map((item) => item.source_key_text));
+  }
 
   // show question
+  private showQuestion(item?: Item) {
+    if (!item) {
+      return;
+    }
+    const question = `Question: ${item.source_key_text}\n`;
+    console.log(question);
+  }
 
   // read answer or __skip
   //   correct ? Update date  "score" and next
   //   incorrect ? Update error and date and "next"
   //   __skip ? Update date and "next"
-  // case > nb of item
-  // show result and save y/n
-  // Update file on yes
-  //
-  //
+  private async readAnswer() {
+    console.log("to implement");
+  }
+
   // Correctness strategy
   //    Simple Az===Az
   //    more complicated ? with https://www.npmjs.com/package/fast-diff
+  private isCorrect() {
+    console.log("to implement");
+  }
+
+  // case > nb of item
+  private showResult() {
+    console.log("to implement");
+  }
+
+  // show result and save y/n
+  // Update file on yes
+  private saveResult() {
+    console.log("to implement");
+  }
 
   private async ask(text: string): Promise<string> {
     if (!this.rl) {
--- src/source-json.ts
+++ src/source-json.ts
@@ -1,15 +1,15 @@
-export type Cards =  Record<string, string>;
+export type Cards = Record<string, string>;
 
 export interface Item {
-    source_key_text: string,
-    source_value_text: string,
-    cards: Cards,
-    last_revision: Date,
-    revision_count: number,
-    favorite_lvl: number,
-    error_count: number
+  source_key_text: string;
+  source_value_text: string;
+  cards: Cards;
+  last_revision: Date;
+  revision_count: number;
+  favorite_lvl: number;
+  error_count: number;
 }
 
 export interface SourceJson {
-    items: Item[]
+  items: Item[];
 }
