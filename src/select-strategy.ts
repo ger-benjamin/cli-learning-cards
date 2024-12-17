80da802238b32f0fbf520e1e0c611189f13803e9
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2024-12-17 13:17:13 +0100

    Add random selection strategy

--- src/select-strategy.ts
+++ src/select-strategy.ts
@@ -1,9 +1,17 @@
 import type { Item } from "./source-json.js";
+import shuffle from "lodash/shuffle.js";
+
+export enum Strategies {
+  Date = "date",
+  Random = "random",
+}
 
 /**
  * Get random items via a strategy.
  */
 export class SelectStrategy {
+  private strategy: string = Strategies.Random;
+
   constructor() {}
 
   /**
@@ -11,7 +19,10 @@ export class SelectStrategy {
    * @returns a list of items.
    */
   selectItems(items: Item[], howMany: number): Item[] {
-    return this.selectItemsByDate(items, howMany);
+    if (this.strategy === Strategies.Random) {
+      return this.selectRandomItems(items, howMany);
+    }
+    return this.selectItemsByDate([...items], howMany);
   }
 
   /**
@@ -22,5 +33,14 @@ export class SelectStrategy {
   private selectItemsByDate(items: Item[], howMany: number) {
     items.sort((item1, item2) => +item1.last_revision - +item2.last_revision);
     return items.slice(0, howMany);
+  }
+
+  /**
+   * Get items randomly date.
+   * @returns a list of items.
+   * @private
+   */
+  private selectRandomItems(items: Item[], howMany: number) {
+    return shuffle(items).slice(0, howMany);
   }
 }
